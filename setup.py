64f715705525de7df003c821d4fc4a54699daa7f
Keith Suderman <suderman@cs.vassar.edu>
2017-10-09 13:25:11 -0400

    Initial code import

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'lapps',
+  packages = ['lapps'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'Python API for the Language Applications Grid',
+  author = 'Keith Suderman',
+  author_email = 'suderman@cs.vassar.edu',
+  url = 'https://github.com/lappsgrid-incubator/python-api', # use the URL to the github repo
+  download_url = 'https://github.com/lappsgrid-incubator/python-api/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['lapps', 'nlp', 'web services'], # arbitrary keywords
+  install_requires = ['requests', 'zeep'],
+  classifiers = [],
+)
